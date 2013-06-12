a1a55cf333c9114f53b8a5e1109cad023b4d3f8f
zhouzm <zzm_guitar@163.com>
2013-06-13 04:49:43 +0800

    simple browser

---
+++ src/org/zzmfish/Main.java
@@ -0,0 +1,20 @@
+package org.zzmfish;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class Main extends Activity
+{
+    WebView webview;
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        webview = (WebView)findViewById(R.id.webview);
+        webview.loadUrl("http://www.baidu.com");
+    }
+}
