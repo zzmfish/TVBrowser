1820b7a05966fdbdb5687848fd87464fd936019a
zhouzm <zzm_guitar@163.com>
2013-07-01 15:38:30 +0800

    draw virtual cursor

--- src/org/zzmfish/TVBrowser/MyWebView.java
+++ src/org/zzmfish/TVBrowser/MyWebView.java
@@ -1,6 +1,9 @@
 package org.zzmfish.TVBrowser;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -9,6 +12,7 @@ import android.webkit.WebView;
 public class MyWebView extends WebView
 {
     private static final String TAG = "MyWebView";
+    private Point mCursor = new Point(100, 100);
 
     public MyWebView(Context context) {
         this(context, null);
@@ -22,9 +26,42 @@ public class MyWebView extends WebView
         super(context, attrs, defStyle);
     }
 
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        Log.d(TAG, "onKeyDown(keyCode=" + keyCode + ")");
-        return false;
-    }
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		Log.d(TAG, "MyWebView.onKeyDown");
+		switch (keyCode) {
+		case KeyEvent.KEYCODE_DPAD_LEFT:
+			if (mCursor.x > 0)
+				mCursor.x -= 10;
+			break;
+		case KeyEvent.KEYCODE_DPAD_RIGHT:
+			if (mCursor.x < getWidth() - 1)
+				mCursor.x += 10;
+			break;
+		case KeyEvent.KEYCODE_DPAD_UP:
+			if (mCursor.y > 0)
+				mCursor.y -= 10;
+			break;
+		case KeyEvent.KEYCODE_DPAD_DOWN:
+			if (mCursor.y < this.getHeight() - 1)
+				mCursor.y += 10;
+			break;
+		}
+		Log.d("zhouzm", "x=" + mCursor.x + ", y=" + mCursor.y);
+		boolean result = super.onKeyDown(keyCode, event);
+		this.invalidate();
+		return result;
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		Log.d(TAG, "MyWebView.onDraw");
+		super.onDraw(canvas);
+		Paint paint = new Paint();
+		paint.setColor(0xffff0000);
+		canvas.drawCircle(mCursor.x, mCursor.y, 3, paint);
+	}
+
+
 }
 
